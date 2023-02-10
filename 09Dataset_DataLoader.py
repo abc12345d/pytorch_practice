1081e847d4c9126c56f04c65fa73ac28989681b6
abc12345d <zhiqi@Zhis-MacBook-Pro.local>
2023-02-10 18:11:12 +0000

    add code

--- 09Dataset_DataLoader.py
+++ 09Dataset_DataLoader.py
@@ -1,3 +1,4 @@
+#%%
 import torch
 import torchvision
 from torch.utils.data import Dataset, DataLoader
@@ -15,3 +16,45 @@ path_wine_data = "./data/wine.csv"
 #     - __len__(self)             # so that we can len(dataset)
 # (2) initialise a DataLoader where dataset = the instance of our custom Dataset object
 #     - try iterate the DataLoader
+
+class Wine_Dataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        wine_data = np.loadtxt(path_wine_data, delimiter=",",dtype= np.float32, skiprows=1)
+        
+        # first column is the class label, the rest are the features
+        self.y = torch.from_numpy(wine_data[:, [0]]) # shape = (no_samples, n_features)
+        self.X = torch.from_numpy(wine_data[:, 1:]) # shape = (no_samples, 1)
+        self.no_samples = self.y.shape[0]
+
+    def __getitem__(self, index):
+        return self.y[index], self.X[index]
+
+    def __len__(self):
+        return self.no_samples
+
+wine_data = Wine_Dataset()
+batch_size = 20
+wine_dataloader = DataLoader(dataset = wine_data, batch_size = batch_size, 
+                            shuffle = True, num_workers = 2)
+
+wine_iterator = iter(wine_dataloader)
+data = next(wine_iterator)
+features, labels = data
+print(features, labels)
+
+no_iter = 0
+for batch in wine_dataloader:
+    y, x = batch
+    no_iter += 1
+
+# %%
+# test case: 
+if (no_iter * batch_size) >= len(wine_dataloader):
+    print(f"test case: {True}")
+else:
+    print(f"test case: {False}")
+
+
+
+# %%
--- 10DatasetTransforms.py
+++ 10DatasetTransforms.py
@@ -1,6 +1,10 @@
+#%%
 import torch
 import torchvision
 import numpy as np
+from torch.utils.data import Dataset
+
+path_wine_data = "./data/wine.csv"
 # TODO: 
 # (1) implement ToTensor class which convert ndarrays to Tensors
 #     - have __call__(self,sample)
@@ -10,3 +14,52 @@ import numpy as np
 #     - without transform
 #     - with toTensor transform 
 #     - with toTensor and Multiplication transform
+class ToTensor():
+    # Convert ndarrays to Tensors
+    def __call__(self, sample):
+        y, x = sample
+        return torch.from_numpy(y), torch.from_numpy(x)
+
+class MulTransform():
+    # multiply inputs with a given factor
+    def __init__(self, factor):
+        self.factor = factor
+    
+    def __call__(self,sample):
+        y, x = sample
+        return y, x * self.factor
+
+class Wine_Dataset(Dataset):
+    def __init__(self, transform = None):
+        super().__init__()
+        wine_data = np.loadtxt(path_wine_data, delimiter=",",dtype= np.float32, skiprows=1)
+        
+        self.y = wine_data[:, [0]]
+        self.x = wine_data[:, 1:]
+        self.no_samples = self.y.shape[0]
+        self.transform = transform
+
+
+    def __getitem__(self, index):
+        sample = self.y[index], self.x[index]
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def __len__(self):
+        return self.no_samples
+
+
+npToTensor = ToTensor()
+mulTrans = MulTransform(2)
+composed = torchvision.transforms.Compose([npToTensor,mulTrans])
+
+wine_data_without = Wine_Dataset()
+print(f"without: {wine_data_without[0]}")
+wine_data_npToTensor = Wine_Dataset(transform = npToTensor)
+print(f"toTensor: {wine_data_npToTensor[0]}")
+wine_data_mulTrans = Wine_Dataset(transform = mulTrans)
+print(f"mulTrans: {wine_data_mulTrans[0]}")
+wine_data_composed = Wine_Dataset(transform = composed)
+print(f"composed: {wine_data_composed[0]}")
